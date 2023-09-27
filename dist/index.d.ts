273b76da5c0d378105f140e2965ba78e3a3166b3
Rom Grk <romgrk.cc@gmail.com>
2023-09-27 19:29:43 -0400

    update

--- dist/index.d.ts
+++ dist/index.d.ts
@@ -1,3 +1,6 @@
 export * from './classes';
+export * from './algorithms/boolean_op';
+export * from './algorithms/distance';
 export * from './algorithms/intersection';
+export * from './algorithms/ray_shooting';
 //# sourceMappingURL=index.d.ts.map
--- dist/index.js
+++ dist/index.js
@@ -1,4 +1,7 @@
 import { _setupShape, point } from './classes';
 export * from './classes';
+export * from './algorithms/boolean_op';
+export * from './algorithms/distance';
 export * from './algorithms/intersection';
+export * from './algorithms/ray_shooting';
 _setupShape(point);
