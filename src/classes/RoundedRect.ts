837be76aab989c638ebb30654d9767d635bd6a77
Rom Grk <romgrk.cc@gmail.com>
2024-04-12 06:58:59 -0400

    fix: rounded rect

--- src/classes/RoundedRect.ts
+++ src/classes/RoundedRect.ts
@@ -11,16 +11,13 @@ export class RoundedRect extends Polygon {
   constructor(x: number, y: number, width: number, height: number, r: number) {
     const edges = [
       segment(x + r, y, x + width - r, y),
-      arc(point(x + width - r, y + r), r, TAU * 0.75, TAU, false),
-
+      arc(point(x + width - r, y + r), r, TAU * 0.75, TAU, true),
       segment(x + width, y + r, x + width, y + height - r),
-      arc(point(x + width - r, y + height - r), r, TAU * 0, TAU * 0.25, false),
-
+      arc(point(x + width - r, y + height - r), r, TAU * 0, TAU * 0.25, true),
       segment(x + width - r, y + height, x + r, y + height),
-      arc(point(x + r, y + height - r), r, TAU * 0.25, TAU * 0.5, false),
-
+      arc(point(x + r, y + height - r), r, TAU * 0.25, TAU * 0.5, true),
       segment(x, y + height - r, x, y + r),
-      arc(point(x + r, y + r), r, TAU * 0.5, TAU * 0.75, false),
+      arc(point(x + r, y + r), r, TAU * 0.5, TAU * 0.75, true),
     ]
     super(edges)
   }
